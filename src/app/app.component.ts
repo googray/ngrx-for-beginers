b1fc768c1296e97f0cc24586ba26913155815926
googray <shat.rom07@gmail.com>
2022-11-17 22:13:25 +0100

    NGRX-1 add counter code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngNew-app';
+  // title = 'ngNew-app';
+  counterDyn = 0;
+  updatedAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counterDyn <= 0;
+  }
+
+  incr(): void {
+    this.updatedAt = Date.now();
+    this.counterDyn++;
+  }
+  decr(): void {
+    this.updatedAt = Date.now();
+    this.counterDyn--;
+  }
+  clear(): void {
+    this.updatedAt = Date.now();
+    this.counterDyn = 0;
+  }
 }
